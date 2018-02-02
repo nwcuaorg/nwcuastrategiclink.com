99d11025b24071e12a70a790cfa89a78ebb7a917
James Pederson <james@jpederson.com>
2018-02-02 16:23:18 -0600

    Dimensions of post thumbnails on the link.

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ get_header(); ?>
 				?>
 				<hr>
 				<div class="entry group">
-					<?php the_post_thumbnail(); ?>
+					<?php the_post_thumbnail( array( 350, 200 ) ); ?>
 					<h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
 					<?php the_excerpt(); ?>
 				</div>
