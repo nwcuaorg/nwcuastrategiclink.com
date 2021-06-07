11cc2a63dfb3e293b60c608f31b8143a46bfc640
James Pederson <james@jpederson.com>
2021-06-07 18:12:00 -0500

    Set homepage partner solutions list to hardcoded.

--- page-front.php
+++ page-front.php
@@ -17,7 +17,54 @@ $featured_partner = get_cmb_value( 'home_featured_partner' );
 		<div class="wrap">
 			<h2>Partner Solutions</h2>
 		</div>
-		<?php the_product_category_list() ?>
+		<a href="/card-service-payment/"><div class="solution bg-grey-light">
+		<div class="solution-icon">
+		<img data-src="/wp-content/uploads/2015/03/icon-card-services.png" src="/wp-content/uploads/2015/03/icon-card-services.png">
+		</div>
+		<h3>Card Services</h3>
+		</div></a>
+		<a href="/employee-benefits/"><div class="solution bg-grey-dark">
+		<div class="solution-icon">
+		<img data-src="/wp-content/uploads/2015/03/icon-hr.png" src="/wp-content/uploads/2015/03/icon-hr.png">
+		</div>
+		<h3>Human Resources</h3>
+		</div></a>
+		<a href="/emerging-technology/"><div class="solution bg-teal">
+		<div class="solution-icon">
+		<img data-src="/wp-content/uploads/2015/03/icon-innovation.png" src="/wp-content/uploads/2015/03/icon-innovation.png">
+		</div>
+		<h3>Innovation</h3>
+		</div></a>
+		<a href="/insurance/"><div class="solution bg-lime">
+		<div class="solution-icon">
+		<img data-src="/wp-content/uploads/2015/03/icon-insurance.png" src="/wp-content/uploads/2015/03/icon-insurance.png">
+		</div>
+		<h3>Insurance</h3>
+		</div></a>
+		<a href="/lending-collections/"><div class="solution bg-navy">
+		<div class="solution-icon">
+		<img data-src="/wp-content/uploads/2015/03/icon-lending.png" src="/wp-content/uploads/2015/03/icon-lending.png">
+		</div>
+		<h3>Lending &amp; Collections</h3>
+		</div></a>
+		<a href="/member-services/"><div class="solution bg-river">
+		<div class="solution-icon">
+		<img data-src="/wp-content/uploads/2015/03/icon-member-services.png" src="/wp-content/uploads/2015/03/icon-member-services.png">
+		</div>
+		<h3>Member Services</h3>
+		</div></a>
+		<a href="/mobile-enhancements/"><div class="solution bg-forest">
+		<div class="solution-icon">
+		<img data-src="/wp-content/uploads/2015/03/icon-mobile-banking.png" src="/wp-content/uploads/2015/03/icon-mobile-banking.png">
+		</div>
+		<h3>Mobile Banking</h3>
+		</div></a>
+		<a href="/operations/"><div class="solution bg-grey-light">
+		<div class="solution-icon">
+		<img data-src="/wp-content/uploads/2015/03/icon-operations.png" src="/wp-content/uploads/2015/03/icon-operations.png">
+		</div>
+		<h3>Operations</h3>
+		</div></a>
 	</div>
 
 	<div class="wrap group">	
